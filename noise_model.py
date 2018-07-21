79f468f1d9ee5f198857f67d9b385ef360b44d9a
yusuke-a-uchida <yusuke.a.uchida@dena.com>
2018-07-22 01:50:05 +0900

    test noise model

--- noise_model.py
+++ noise_model.py
@@ -1,4 +1,6 @@
+import argparse
 import numpy as np
+import cv2
 
 
 def get_noise_model(noise_type="gaussian,0,50"):
@@ -18,3 +20,33 @@ def get_noise_model(noise_type="gaussian,0,50"):
         return gaussian_noise
     elif tokens[0] == "clean":
         return lambda img: img
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="test noise model",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--image_size", type=int, default=256,
+                        help="training patch size")
+    parser.add_argument("--noise_model", type=str, default="gaussian,0,50",
+                        help="noise model to be tested")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+    image_size = args.image_size
+    noise_model = get_noise_model(args.noise_model)
+
+    while True:
+        image = np.ones((image_size, image_size, 3), dtype=np.uint8) * 128
+        cv2.imshow("noise image", noise_model(image))
+        key = cv2.waitKey(-1)
+
+        # "q": quit
+        if key == 113:
+            return 0
+
+
+if __name__ == '__main__':
+    main()
